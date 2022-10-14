a22aff6b7c378a0b643705f0afdfe9ea093890e5
chavdar77 <chavmom@gmail.com>
2022-10-14 22:59:04 +0300

    Changes to be committed: 	modified:   front-end/src/employees/AddEmployee.js

--- front-end/src/employees/AddEmployee.js
+++ front-end/src/employees/AddEmployee.js
@@ -7,10 +7,34 @@ export default function AddEmployee() {
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow" >
             <h2 className="text-center m-4">Register Employee</h2>
             <div className="mb-3">
-              <label htmlFor="Name" className="form-label">
-                Name
+              <label htmlFor="FirstName" className="form-label">
+                First Name
               </label>
-
+              <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter you first name"
+              name="firstName"/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="LastName" className="form-label">
+                Last Name
+              </label>
+              <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter you last name"
+              name="lastName"/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                E-mail
+              </label>
+              <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your e-mail address"
+              name="email"/>
             </div>
           </div>  
       </div>
