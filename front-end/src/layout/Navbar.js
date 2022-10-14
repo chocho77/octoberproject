855bcdaf4fa58c641b69eeae4a151927c6d201fd
chavdar77 <chavmom@gmail.com>
2022-10-14 22:31:46 +0300

    Changes to be committed: 	modified:   front-end/src/layout/Navbar.js

--- front-end/src/layout/Navbar.js
+++ front-end/src/layout/Navbar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div>
@@ -12,7 +12,7 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Add Employee</a>
+          <a className="nav-link active" aria-current="page" href="#"><Link to="/addEmployee">Add Employee</Link></a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Link</a>
