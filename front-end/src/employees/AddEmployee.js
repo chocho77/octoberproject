d9eed7204dd8603f21b866657f4ddd6f63587913
chavdar77 <chavmom@gmail.com>
2022-10-14 23:04:11 +0300

    Changes to be committed: 	modified:   front-end/src/employees/AddEmployee.js

--- front-end/src/employees/AddEmployee.js
+++ front-end/src/employees/AddEmployee.js
@@ -36,6 +36,12 @@ export default function AddEmployee() {
               placeholder="Enter your e-mail address"
               name="email"/>
             </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <button type="submit" className="btn btn-outline-danger mx-2">
+              Cancel
+            </button>
           </div>  
       </div>
 
