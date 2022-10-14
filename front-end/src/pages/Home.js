b7f4eb06c50bb8c586190e42fb72c62b2be76110
chavdar77 <chavmom@gmail.com>
2022-10-14 10:16:26 +0300

    Changes to be committed: 	modified:   front-end/src/pages/Home.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='container'>
+        <div className='py-4'>
+            
+        </div>
+
+    </div>
   )
 }
