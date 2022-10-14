b3a24c4cf7a234196fa7e0b99213a9dcac2cd1db
chavdar77 <chavmom@gmail.com>
2022-10-14 22:39:52 +0300

    Changes to be committed: 	modified:   front-end/src/employees/AddEmployee.js

--- front-end/src/employees/AddEmployee.js
+++ front-end/src/employees/AddEmployee.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function AddEmployee() {
   return (
-    <div>AddEmployee Form</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow" >
+
+        </div>
+      </div>
+
+    </div>
   )
 }
