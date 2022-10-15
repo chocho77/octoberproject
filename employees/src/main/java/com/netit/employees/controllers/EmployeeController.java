20488a7fd68f3144e822a97ab58b40e5e37371f2
chavdar77 <chavmom@gmail.com>
2022-10-15 12:33:38 +0300

    Changes to be committed: 	modified:   employees/src/main/java/com/netit/employees/controllers/EmployeeController.java 	new file:   employees/src/main/java/com/netit/employees/exception/EmployeeNotFoundAdvice.java 	new file:   employees/src/main/java/com/netit/employees/exception/EmployeeNotFoundException.java

--- employees/src/main/java/com/netit/employees/controllers/EmployeeController.java
+++ employees/src/main/java/com/netit/employees/controllers/EmployeeController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netit.employees.exception.EmployeeNotFoundException;
 import com.netit.employees.models.Employee;
 import com.netit.employees.repository.EmployeeRepository;
 
@@ -28,6 +32,34 @@ public class EmployeeController {
     @GetMapping("/employees")
     List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+    
+    @GetMapping("/employee/{id}")
+    Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(()-> new EmployeeNotFoundException(id));
+    }
+    
+    @PutMapping("/employee/{id}")
+    Employee updateEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return employeeRepository.findById(id)
+                .map(employee -> {
+                    employee.setFirstName(newEmployee.getFirstName());
+                    employee.setLastName(newEmployee.getLastName());
+                    employee.setEmail(newEmployee.getEmail());
+                    return employeeRepository.save(employee);
+                }).orElseThrow(()-> new EmployeeNotFoundException(id));
+        
+    }
+    
+    @DeleteMapping("/employee/{id}")
+    String deleteEmployee(@PathVariable Long id) {
+        if(!employeeRepository.existsById(id)) {
+            throw new EmployeeNotFoundException(id);
+        }
+        employeeRepository.deleteById(id);
+        return "User with id " + id + " has been deleted success.";
+        
     }
     
 
---
+++ employees/src/main/java/com/netit/employees/exception/EmployeeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.netit.employees.exception;
+
+public class EmployeeNotFoundException extends RuntimeException{
+    
+    public EmployeeNotFoundException(Long id) {
+        super("Could not found the employee with id " + id);
+        
+    }
+
+}
