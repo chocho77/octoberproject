65b85fa17f45da79863e52e0aeb85609fbcc37b1
chavdar77 <chavmom@gmail.com>
2022-10-16 16:06:20 +0300

    Changes to be committed: 	modified:   front-end/src/pages/Home.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams} from "react-router-dom";
 
 export default function Home() {
     const [employee, setEmployee] = useState([]);
