358986fcffdfda81d533569cebd821f24649ccc4
chavdar77 <chavmom@gmail.com>
2022-10-16 13:01:11 +0300

    Changes to be committed: 	modified:   front-end/src/employees/ViewEmployee.js

--- front-end/src/employees/ViewEmployee.js
+++ front-end/src/employees/ViewEmployee.js
@@ -3,8 +3,12 @@ import React from "react";
 export default function ViewEmployee() {
 
     return (
-        <div>
-
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                      
+                </div>
+            </div>
         </div>
     );
 }
