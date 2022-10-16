63040c60d0b6872260140912d6d7f3d5b8aa83de
chavdar77 <chavmom@gmail.com>
2022-10-16 13:03:32 +0300

    Changes to be committed: 	modified:   front-end/src/employees/ViewEmployee.js

--- front-end/src/employees/ViewEmployee.js
+++ front-end/src/employees/ViewEmployee.js
@@ -6,7 +6,7 @@ export default function ViewEmployee() {
         <div className="container">
             <div className="row">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                      
+                      <h2 className="text-center m-4">Employee Details</h2>
                 </div>
             </div>
         </div>
