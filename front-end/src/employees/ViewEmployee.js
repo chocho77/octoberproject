fc17e029af607d84732a6878a30932edafe4c8ac
chavdar77 <chavmom@gmail.com>
2022-10-16 13:07:25 +0300

    Changes to be committed: 	modified:   front-end/src/employees/ViewEmployee.js

--- front-end/src/employees/ViewEmployee.js
+++ front-end/src/employees/ViewEmployee.js
@@ -7,6 +7,15 @@ export default function ViewEmployee() {
             <div className="row">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                       <h2 className="text-center m-4">Employee Details</h2>
+
+                      <div className="card">
+                        <div className="card-header">
+                            Details of employee id :
+                            <ul className="list-group list-group-flush">
+                                
+                            </ul>
+                        </div>
+                      </div>
                 </div>
             </div>
         </div>
